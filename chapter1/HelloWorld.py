90f2e552745a105a6b050485629c0a47ff0c0fb8
VincentJ <rogue1990@163.com>
2018-04-22 02:47:12 +0800

    Initial commit.

---
+++ chapter1/HelloWorld.py
@@ -0,0 +1,45 @@
+print("Hello Python World")
+
+message = "Hello Python"
+print(message)
+message = "Hello Python3.6"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+firstName = "ada"
+lastName = "lovelace"
+fullName = firstName + lastName
+print(fullName)
+
+allClasses = "Classes:\n\tChinese\n\tEnglish\n\tMath"
+print(allClasses)
+
+strWithBlank = 'Are U With Blanks?    --Yes    '
+# 去首尾空格--去中间的呢?
+print(strWithBlank.rstrip())
+
+msg = "One of Python's strengths is its diverse community."
+print(msg)
+#  msg='One of Python's strengths is its diverse community.'
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+
+print(3 ** 2)
+print(2 + 2 ** 3)
+
+# 浮点数字
+print(2 * 0.2)
+print(0.1 + 0.4)
+print(0.2 + 0.1)
+print(3 * 0.1)
+print(3 / 2)
+age = 23
+hb = "Happy " + str(age) + "rd birthday!"
+print(hb)
